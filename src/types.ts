ee5a297ab73a0bdd145e0a969c2abd9ea4322747
Alexander <aguillenhernandez95@gmail.com>
2023-03-09 18:15:07 +0100

    Add LoginTokenPayload, ErroResponse and UserCredentials types

--- src/types.ts
+++ src/types.ts
@@ -1,11 +1,22 @@
-export interface User {
+export interface LoginTokenPayload {
   id: string;
   username: string;
+}
+export interface UserCredentials {
+  username: string;
+  password: string;
+}
+
+export interface User extends LoginTokenPayload {
   token: string;
 }
 
 export interface TokenResponse {
   token: string;
+}
+
+export interface ErrorResponse {
+  error: string;
 }
 
 export interface UserState extends User {
