3d684ce38353fd16b959281c5298f39925530d1c
Alexander <aguillenhernandez95@gmail.com>
2023-03-11 00:43:21 +0100

    Add font loading and splash screen to App

--- App.tsx
+++ App.tsx
@@ -1,25 +1,39 @@
-import React from "react";
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useCallback, useEffect, useState } from "react";
+import { View } from "react-native";
 import { Provider } from "react-redux";
 import { store } from "./src/store/store";
+import CredentialsForm from "./src/components/CredentialsForm/CredentialsForm";
+import useFonts from "./src/styles/useFonts";
+import * as SplashScreen from "expo-splash-screen";
 
 export default function App() {
+  const [isAppReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        await useFonts();
+      } finally {
+        setAppIsReady(true);
+      }
+    })();
+  }, [isAppReady, useFonts]);
+
+  const showLayoutRootView = useCallback(async () => {
+    if (isAppReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [isAppReady]);
+
+  if (!isAppReady) {
+    return null;
+  }
+
   return (
     <Provider store={store}>
-      <View style={styles.container}>
-        <Text>Open up App.tsx to start working on!</Text>
-        <StatusBar style="auto" />
+      <View style={{ flex: 1 }} onLayout={showLayoutRootView}>
+        <CredentialsForm />
       </View>
     </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
