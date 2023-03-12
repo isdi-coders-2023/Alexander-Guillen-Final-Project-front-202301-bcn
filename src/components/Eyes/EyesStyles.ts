01bc1f716d4076261eb814156839761b954e1751
Alexander <aguillenhernandez95@gmail.com>
2023-03-12 17:44:01 +0100

    Add Eyes component styles

---
+++ src/components/Eyes/EyesStyles.ts
@@ -0,0 +1,17 @@
+import { StyleSheet } from "react-native";
+
+const EyesStyles = StyleSheet.create({
+  eyesContainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "flex-end",
+    gap: 101,
+  },
+  eye: {
+    width: 70,
+    height: 70,
+  },
+});
+
+export default EyesStyles;
