f015a35bf790acbcd9f45b1e919b34c9b87d6be5
Alexander <aguillenhernandez95@gmail.com>
2023-03-16 19:48:22 +0100

    Add new test for useToken when .getItem returns null

--- src/hooks/useToken/useToken.test.tsx
+++ src/hooks/useToken/useToken.test.tsx
@@ -10,11 +10,13 @@ import { type User } from "../../types";
 
 jest.mock("jwt-decode", () => jest.fn());
 
-AsyncStorage.getItem = jest.fn().mockReturnValue(mockToken);
-
 describe("Given a useToken hook", () => {
-  describe("When getToken is called", () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+  describe("When getToken is called and there's a token in the storage", () => {
     test("Then it should call dispatch with loginUser action", async () => {
+      AsyncStorage.getItem = jest.fn().mockReturnValue(mockToken);
       const user: User = {
         ...tokenPayload,
         token: mockToken,
@@ -38,6 +40,26 @@ describe("Given a useToken hook", () => {
       await getToken();
 
       expect(dispatchSpy.mock.calls[0][0]).toStrictEqual(loginUserAction);
+    });
+  });
+
+  describe("When getToken is called there's not a token in the storage", () => {
+    test("Then it shouldn't call dispatch", async () => {
+      AsyncStorage.getItem = jest.fn().mockReturnValue(null);
+      const dispatchSpy = jest.spyOn(testStore, "dispatch");
+      const {
+        result: {
+          current: { getToken },
+        },
+      } = renderHook(() => useToken(), {
+        wrapper({ children }) {
+          return <Provider store={testStore}>{children}</Provider>;
+        },
+      });
+
+      await getToken();
+
+      expect(dispatchSpy).not.toHaveBeenCalled();
     });
   });
 });
