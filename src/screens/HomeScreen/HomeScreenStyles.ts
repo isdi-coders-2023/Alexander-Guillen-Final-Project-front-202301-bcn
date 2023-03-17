abaa70fe00919bc56aa0b132ec0ef0abef85a6bd
Alexander <aguillenhernandez95@gmail.com>
2023-03-17 13:30:19 +0100

    Remove StyleProp and TextStyle imports

--- src/screens/HomeScreen/HomeScreenStyles.ts
+++ src/screens/HomeScreen/HomeScreenStyles.ts
@@ -1,4 +1,4 @@
-import { type StyleProp, StyleSheet, type TextStyle } from "react-native";
+import { StyleSheet } from "react-native";
 
 const HomeScreenStyles = StyleSheet.create({
   mainContainer: {
