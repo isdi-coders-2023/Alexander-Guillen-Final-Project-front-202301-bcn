d84aae2483fe2c466e8ce93a32741dccca016b9f
Alexander <aguillenhernandez95@gmail.com>
2023-03-20 10:25:40 +0100

    Style DeleteOrCreateAction component

---
+++ src/components/DeleteOrCreateAction/DeleteOrCreateActionStyles.ts
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+const DeleteOrCreateActionStyles = StyleSheet.create({
+  delete: {
+    position: "absolute",
+    zIndex: 100,
+    left: "80.89%",
+    top: "2%",
+  },
+});
+
+export default DeleteOrCreateActionStyles;
